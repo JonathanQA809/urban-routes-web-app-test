c038d3d4e820c4ed9b2df59380ea777828301e6e
Jonathan Enrique Tejeda <askjonathantejeda@gmail.com>
2025-08-14 21:46:14 -0400

    Project 7 second commit

--- data.py
+++ data.py
@@ -1,1 +1,11 @@
+# URL for the Urban Routes application
+URBAN_ROUTES_URL = ''
+
+# Test data constants
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
 # File with constants
